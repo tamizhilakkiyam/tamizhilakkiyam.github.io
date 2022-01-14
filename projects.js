faf9072271d921a0cf34843b533948e95fe8cf24
scientific-dev <69678617+Scientific-Guy@users.noreply.github.com>
2022-01-14 22:01:33 +0530

    feat: initial commit

---
+++ projects.js
@@ -0,0 +1,16 @@
+window.projects = [
+    {
+        name: "Aathichudi",
+        tamil: "ஆத்திசூடி",
+        href: "aathichudi",
+        description: "A site to display Āathichoodi (ஆத்திசூடி), a collection of single-line quotations written by Avvaiyar and organized in alphabetical order. There are 109 of these sacred lines.",
+        tamilDescription: "ஔவையாரின் ஆத்திசூடியை எளிதாகக்காட்ட ஓர் இணையதளம்."
+    },
+    {
+        name: "Tirukkural",
+        tamil: "திருக்குறள்",
+        href: "tirukural",
+        description: "A site to display Tirukkuṟaḷ (திருக்குறள்), a classic Tamil language text consisting of 1,330 short couplets, or kurals, of seven words each. It is considered one of the greatest works ever written on ethics and morality, it is known for its universality and secular nature.",
+        tamilDescription: "திருவள்ளுவரின் திருக்குறளை எளிதாகக்காட்ட ஓர் இணையதளம்."
+    }
+]
